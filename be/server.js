74f7fb13bf9585cbb6de4f293575b80ab8b837b2
TLChouny <103498836+TLChouny@users.noreply.github.com>
2025-06-04 23:56:33 +0700

    Update server.js

--- be/server.js
+++ be/server.js
@@ -14,13 +14,13 @@ app.use(cors({
   origin: "http://localhost:5173",
   credentials: true,
 }));
-app.use(express.json());
+
 
 app.use(cors({
   origin: 'https://wdp-301-hiv-health-care-web.vercel.app/',
   credentials: true,
 }));
-
+app.use(express.json());
 // Routes
 app.use('/api', routes);
 

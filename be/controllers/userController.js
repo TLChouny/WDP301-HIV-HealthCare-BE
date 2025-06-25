4714cb9ea76c44c051180cf17893d0864cf9fdc6
TLChouny <103498836+TLChouny@users.noreply.github.com>
2025-06-25 14:53:22 +0700

    thêm userName khi login

--- be/controllers/userController.js
+++ be/controllers/userController.js
@@ -173,7 +173,7 @@ exports.login = async (req, res) => {
       return res.status(401).json({ message: "Invalid credentials" });
     }
 
-    const token = jwt.sign({ id: user._id, role: user.role }, process.env.JWT_SECRET, { expiresIn: "1h" });
+    const token = jwt.sign({ id: user._id, role: user.role, userName: user.userName }, process.env.JWT_SECRET, { expiresIn: "1h" });
     user.accessToken = token;
     user.tokenExpiresAt = new Date(Date.now() + 1 * 60 * 60 * 1000);
     await user.save();
